e99a53fa1222e93702fb9df337671e43bf949534
Lyon Etyo <mulyonoeko.prastyo@gmail.com>
2021-06-24 16:12:51 +0700

    adding chat events and typing events

--- index.js
+++ index.js
@@ -15,4 +15,12 @@ const io = socket(server);
 
 io.on("connection", socket => {
   console.log("Made socket connection", socket.id);
+
+  socket.on("chat", data => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
+  });
 });
--- public/main.js
+++ public/main.js
@@ -1,2 +1,38 @@
 // Make connection
 const socket = io.connect("http://localhost:4000");
+
+// Query the DOM
+const message = document.querySelector(".message");
+const handle = document.querySelector(".name");
+const btn = document.querySelector(".send");
+const output = document.querySelector(".output");
+const feedback = document.querySelector(".feedback p");
+const chatMessage = document.createElement("p");
+const strong = document.createElement("strong");
+const emphasize = document.createElement("em");
+
+// Emit events
+btn.addEventListener("click", _ => {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value,
+  });
+  message.value = "";
+  handle.disabled = true;
+});
+
+// emit typing status
+message.addEventListener("keydown", _ => socket.emit("typing", handle.value));
+
+// Listen for events
+socket.on("chat", data => {
+  feedback.textContent = "";
+  strong.textContent = data.handle;
+  chatMessage.textContent = data.message;
+  chatMessage.insertBefore(strong, chatMessage.firstChild);
+  output.appendChild(chatMessage.cloneNode(true));
+});
+
+socket.on("typing", data => {
+  feedback.textContent = `${data} is typing a message`;
+});
